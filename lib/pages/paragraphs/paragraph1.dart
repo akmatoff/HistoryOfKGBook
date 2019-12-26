ce139c307b5ac600d476b5d34c1440c3f60589a0
akmatoff <azim.akmatov1@gmail.com>
2019-12-26 23:12:07 +0600

    Tables and text

--- lib/pages/paragraphs/paragraph1.dart
+++ lib/pages/paragraphs/paragraph1.dart
@@ -13,28 +13,146 @@ class Paragraph1 extends StatelessWidget {
         SingleChildScrollView(
           child:
         Container(
-          padding: EdgeInsets.all(15.0),
+          padding: EdgeInsets.all(10.0),
           child:
           Column(
             children: [ 
               Text(_paragraph.text,
                 style: TextStyle(
-                  fontFamily: 'Helvetica',
+                  fontFamily: 'Georgia',
+                  fontSize: 18.0,
+                  letterSpacing: 0.5,
+                  color: Colors.black87,
+                ),
+              ),
+              Image.asset(_paragraph.imagePath),
+              Text(_paragraph.text2,
+                style: TextStyle(
+                  fontFamily: 'Georgia',
+                  fontSize: 18.0,
+                  letterSpacing: 0.5,
+                  color: Colors.black87,
+                ),
+              ),
+              Text(_paragraph.text3,
+                style: TextStyle(
+                  fontFamily: 'Georgia',
                   fontSize: 18.0,
                   letterSpacing: 0.5,
                   color: Colors.black87,
                 ),
               ),
               Container(
-                constraints: BoxConstraints.expand(height: 30.0),
-                child: Image.asset(_paragraph.imagePath, fit: BoxFit.cover),
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(5.0),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1.0, color: Colors.black87,),
+                ),
+                child: Text(
+                  _paragraph.table1Header,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Georgia',
+                    fontSize: 17.0,
+                    letterSpacing: 0.5,
+                    color: Colors.black87,
+                  ),
+                ),
               ),
-              Text(_paragraph.text2,
-                style: TextStyle(
-                  fontFamily: 'Helvetica',
-                  fontSize: 18.0,
-                  letterSpacing: 0.5,
-                  color: Colors.black87,
+              SizedBox(height: 15.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    width: 190.0,
+                    padding: EdgeInsets.all(5.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1.0, color: Colors.black87),
+                    ),
+                    child: Text(                  
+                      _paragraph.table1Left,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                    fontFamily: 'Georgia',
+                    fontSize: 17.0,
+                    letterSpacing: 0.5,
+                    color: Colors.black87,
+                      ),
+                    ),
+                    ),
+                    Container(
+                      width: 190.0,
+                    padding: EdgeInsets.all(5.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1.0, color: Colors.black87),
+                    ),
+                    child: Text(                  
+                      _paragraph.table1Right,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                    fontFamily: 'Georgia',
+                    fontSize: 17.0,
+                    letterSpacing: 0.5,
+                    color: Colors.black87,
+                      ),
+                    ),
+                    ),
+                ],
+              ),
+              SizedBox(height: 15.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    width: 190.0,
+                    height: 545.0,
+                    padding: EdgeInsets.all(5.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1.0, color: Colors.black87),
+                    ),
+                    child: Text(                  
+                      _paragraph.table1LeftText,
+                      style: TextStyle(
+                    fontFamily: 'Georgia',
+                    fontSize: 17.0,
+                    letterSpacing: 0.2,
+                    color: Colors.black87,
+                      ),
+                    ),
+                    ),
+                    Container(
+                      width: 190.0,
+                      height: 545.0,
+                      padding: EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1.0, color: Colors.black87),
+                      ),
+                    child: Text(                  
+                      _paragraph.table1RightText,
+                      style: TextStyle(
+                    fontFamily: 'Georgia',
+                    fontSize: 17.0,
+                    letterSpacing: 0.2,
+                    color: Colors.black87,
+                      ),
+                    ),
+                    ),
+                ],
+              ),
+              SizedBox(height: 15.0,),
+              Container(
+                padding: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                        border: Border.all(width: 1.0, color: Colors.black87),
+                      ),
+                child: Text(                  
+                      _paragraph.text4,
+                      style: TextStyle(
+                    fontFamily: 'Georgia',
+                    fontSize: 17.0,
+                    letterSpacing: 0.2,
+                    color: Colors.black87,
+                      ),
                 ),
               ),
         ],
